62dbf24509c78251b3bdf6cb0221a5b8d27d1503
Anh <tranlamtuananh2003@gmail.com>
2023-03-12 01:01:21 -0400

    Created FlightBooked class and centered title in README.md

--- APSS/src/main/java/classes/FlightBooked.java
+++ APSS/src/main/java/classes/FlightBooked.java
@@ -1,4 +1,100 @@
 package classes;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class FlightBooked {
+    private int id;
+    private int flightNo;
+    private LocalDateTime departure;
+    private LocalDateTime arrival;
+    private ArrayList<Luggage> luggage;
+    private double fare;
+    private LocalDateTime delay;
+    private final int LUGGAGE_LIMIT = 3;
+
+    public FlightBooked(int id, int flightNo, LocalDateTime departure, LocalDateTime arrival, ArrayList<Luggage> luggage, double fare, LocalDateTime delay) {
+        this.id = id;
+        this.flightNo = flightNo;
+        this.departure = departure;
+        this.arrival = arrival;
+        if (luggage.size() <= LUGGAGE_LIMIT) {
+            try {
+                this.luggage = luggage;
+            } catch(Exception e) {
+                System.out.println("Luggage number must be <= 3");
+                System.exit(0);
+            }
+        }
+        this.fare = fare;
+        this.delay = delay;
+    }
+
+    public FlightBooked(int id, int flightNo, LocalDateTime departure, LocalDateTime arrival, ArrayList<Luggage> luggage, double fare) {
+        this.id = id;
+        this.flightNo = flightNo;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.luggage = luggage;
+        this.fare = fare;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFlightNo() {
+        return flightNo;
+    }
+
+    public void setFlightNo(int flightNo) {
+        this.flightNo = flightNo;
+    }
+
+    public LocalDateTime getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(LocalDateTime departure) {
+        this.departure = departure;
+    }
+
+    public LocalDateTime getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(LocalDateTime arrival) {
+        this.arrival = arrival;
+    }
+
+    public ArrayList<Luggage> getLuggage() {
+        return luggage;
+    }
+
+    public void setLuggage(ArrayList<Luggage> luggage) {
+        this.luggage = luggage;
+    }
+
+    public double getFare() {
+        return fare;
+    }
+
+    public void setFare(double fare) {
+        this.fare = fare;
+    }
+
+    public LocalDateTime getDelay() {
+        return delay;
+    }
+
+    public void setDelay(LocalDateTime delay) {
+        this.delay = delay;
+    }
 }
+
+
+
