9d6634ca60d24cb5a2202d63b7c08d0ce241c475
Rohit <rbhardw1@unb.ca>
2023-03-15 11:18:44 -0300

    updated the Luggage java class

--- APSS/src/main/java/classes/Luggage.java
+++ APSS/src/main/java/classes/Luggage.java
@@ -1,4 +1,39 @@
 package classes;
 
 public class Luggage {
+    private int tokenNo;
+    private int weight;
+    private String name;
+    private double baseFare;
+
+    public Luggage(int tokenNo, int weight, String name, double baseFare) {
+        this.tokenNo = tokenNo;
+        this.weight = weight;
+        this.name = name;
+        this.baseFare = baseFare;
+    }
+
+    public int getTokenNo() {
+        return tokenNo;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBaseFare() {
+        return baseFare;
+    }
+
+    public void setWeight(int newWeight) {
+        weight = newWeight;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
 }
