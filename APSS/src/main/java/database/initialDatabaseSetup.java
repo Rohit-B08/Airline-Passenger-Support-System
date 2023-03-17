a74626c6d78f35d305a4f3a7edefa68b7fd85434
Rohit <rbhardw1@unb.ca>
2023-03-17 14:59:32 -0300

    updated initialDatabaseSetup file

--- APSS/src/main/java/database/initialDatabaseSetup.java
+++ APSS/src/main/java/database/initialDatabaseSetup.java
@@ -1,7 +1,35 @@
 package database;
+import java.sql.*;
 
 
 public class initialDatabaseSetup {
-
+    static String url = "jdbc:mysql://localhost/";
+    static final String user = "root";
+    static final String pass = "rohit@2002";
+    public static void main(String args[]) throws SQLException {
+        Connection connection;
+        try{
+            connection = DriverManager.getConnection(url, user, pass);
+            Statement stm = connection.createStatement();
+            String showDatabase = "show databases";
+            PreparedStatement preparedStatement = connection.prepareStatement(showDatabase);
+            ResultSet result = preparedStatement.executeQuery();
+            boolean exist = false;
+            while(result.next()) {
+                if(result.getString(1).equals("APSS")) {
+                    exist = true;
+                }
+            }
+            System.out.println(exist);
+            if(!exist) {
+                String createDb = "create database APSS";
+                stm.executeUpdate(createDb);
+                preparedStatement.executeQuery();
+            }
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
