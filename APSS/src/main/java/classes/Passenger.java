d13ca1d9d762be3043a117f8c654a4303aef06c6
Jashanpreet Kaur <jashanp@unb.ca>
2023-03-20 15:38:24 -0300

    Updated Passenger class

--- APSS/src/main/java/classes/Passenger.java
+++ APSS/src/main/java/classes/Passenger.java
@@ -1,4 +1,47 @@
 package classes;
+import java.util.ArrayList;
 
 public class Passenger {
+    private String userName;
+    private String password;
+    private int passengerId;
+    private ArrayList<FlightBooked> listOfBookedFlights;
+
+    public Passenger(String userName, String password, int passengerId) {
+        this.userName = userName;
+        this.password = password;
+        this.passengerId = passengerId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(int passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public ArrayList<FlightBooked> getListOfBookedFlights() {
+        return listOfBookedFlights;
+    }
+
+    public void setListOfBookedFlights(ArrayList<FlightBooked> listOfBookedFlights) {
+        this.listOfBookedFlights = listOfBookedFlights;
+    }
 }
