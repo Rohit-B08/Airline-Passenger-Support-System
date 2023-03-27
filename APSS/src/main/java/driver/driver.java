b88b3f7ae80322a2f6e7bf7d907c7499a51512c7
Rohit <rbhardw1@unb.ca>
2023-03-27 16:28:29 -0300

    made some changes

--- APSS/src/main/java/driver/driver.java
+++ APSS/src/main/java/driver/driver.java
@@ -6,31 +6,41 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.FileNotFoundException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class driver extends Application {
     public void start(Stage stage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("APSS/src/main/java/guis/loginScreen.fxml"));
+            initialDatabaseSetup connection = new initialDatabaseSetup("rohit@2002");
+//            connection.initializeDatabase();
+//            connection.resetDataBase();
+            Parent root = FXMLLoader.load(getClass().getResource("loginScreen.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
 //    public static void main(String[] args) throws SQLException {
 //        Scanner scanner = new Scanner(System.in);
-////            System.out.print("Please enter your password for mysql database : ");
+//            System.out.print("Please enter your password for mysql database : ");
 //
-////            String pass = scanner.next();
+//            String pass = scanner.next();
 //            try {
 //                initialDatabaseSetup connection = new initialDatabaseSetup("rohit@2002");
 //                connection.initializeDatabase();
-////                connection.resetDataBase();
+//                connection.resetDataBase();
+//                ResultSet result = connection.passengerTable();
+//                while (result.next()) {
+//                    System.out.print(result.getString(2) + "\t");
+//                    System.out.println(result.getString(3));
+//                }
 //            }
 //            catch (Exception e) {
 //                e.printStackTrace();
