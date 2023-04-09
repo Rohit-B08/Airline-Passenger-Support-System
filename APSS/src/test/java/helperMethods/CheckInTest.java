f6be31219a6b5ddfd03f3467df2b92e6ac314dda
gopikashrivastav <gopikashrivastav@Gopikas-MacBook-Air.local>
2023-04-09 00:28:46 -0300

    Changes to checkInTest class, test now works

--- APSS/src/test/java/helperMethods/CheckInTest.java
+++ APSS/src/test/java/helperMethods/CheckInTest.java
@@ -14,12 +14,12 @@ class CheckInTest {
     CheckIn checkIn;
     @BeforeEach
     void setUp() {
-        checkIn = new CheckIn("welcomepm3");
+        checkIn = new CheckIn("chAtEAUx45671234!");
     }
 
     @Test
     void checkInTest() throws SQLException {
-        String itineraryNum = "IT299";
+        String itineraryNum = "IT234";
         int passengerId = 0;
         String departure = "";
         String arrival = "";
@@ -36,13 +36,13 @@ class CheckInTest {
             arrival = boardingInfo.getString(3);
         }
 
-        String expectedOutput = "****************************************************************\r\n";
-        expectedOutput += "                          "+ passengerId + "                           \r\n";
-        expectedOutput += "   Departure                                        Arrival     \r\n";
-        expectedOutput += "   "+departure+"                                   \t\t"+arrival+"   \r\n";
-        expectedOutput += "                        ItenaryNumber                           \r\n";
-        expectedOutput += "                          " + itineraryNum + "                          \r\n";
-        expectedOutput += "****************************************************************\r\n";
+        String expectedOutput = "****************************************************************" + "\n"
+                                + String.format("%27s", passengerId) + String.format("%25s" , "\n")
+                                + String.format("%7s", "Departure") + String.format("%41s", "Arrival") + String.format("%4s" , "\n")
+                                + String.format("%5s", departure) + String.format("%43s" , arrival) + String.format("%4s" , "\n")
+                                + String.format("%33s", "Itinerary Number") + String.format("%15s" , "\n")
+                                + String.format("%27s" , itineraryNum) + String.format("%15s", "\n")
+                                + "****************************************************************" + "\n";
 
         assertEquals(expectedOutput, outContent.toString());
     }
